15822e7d7d87cd16cf6c6f537a3cf7c4c88eebc5
121210718 LUANA DOS SANTOS LIBERATO <luana.liberatox@gmail.com>
2022-11-10 15:16:13 -0300

    Primeiras implementações do Roteiro 4

---
+++ Roteiro 4/top3.sv
@@ -0,0 +1,57 @@
+// DESCRIPTION: Verilator: Systemverilog example module
+// Roteiro 4 - ROM - Luana dos Santos Liberato
+
+parameter divide_by=100000000;  // divisor do clock de referência
+// A frequencia do clock de referencia é 50 MHz.
+// A frequencia de clk_2 será de  50 MHz / divide_by
+
+parameter NBITS_INSTR = 32;
+parameter NBITS_TOP = 8, NREGS_TOP = 32, NBITS_LCD = 64;
+module top(input  logic clk_2,
+           input  logic [NBITS_TOP-1:0] SWI,
+           output logic [NBITS_TOP-1:0] LED,
+           output logic [NBITS_TOP-1:0] SEG,
+           output logic [NBITS_LCD-1:0] lcd_a, lcd_b,
+           output logic [NBITS_INSTR-1:0] lcd_instruction,
+           output logic [NBITS_TOP-1:0] lcd_registrador [0:NREGS_TOP-1],
+           output logic [NBITS_TOP-1:0] lcd_pc, lcd_SrcA, lcd_SrcB,
+             lcd_ALUResult, lcd_Result, lcd_WriteData, lcd_ReadData, 
+           output logic lcd_MemWrite, lcd_Branch, lcd_MemtoReg, lcd_RegWrite);
+
+  always_comb begin
+    //SEG <= SWI;
+    lcd_WriteData <= SWI;
+    lcd_pc <= 'h12;
+    lcd_instruction <= 'h34567890;
+    lcd_SrcA <= 'hab;
+    lcd_SrcB <= 'hcd;
+    lcd_ALUResult <= 'hef;
+    lcd_Result <= 'h11;
+    lcd_ReadData <= 'h33;
+    lcd_MemWrite <= SWI[0];
+    lcd_Branch <= SWI[1];
+    lcd_MemtoReg <= SWI[2];
+    lcd_RegWrite <= SWI[3];
+    for(int i=0; i<NREGS_TOP; i++)
+       if(i != NREGS_TOP/2-1) lcd_registrador[i] <= i+i*16;
+       else                   lcd_registrador[i] <= ~SWI;
+    lcd_a <= {56'h1234567890ABCD, SWI};
+    lcd_b <= {SWI, 56'hFEDCBA09876543};
+  end
+
+  parameter DADO_ZERO_ZERO = 'b0011; 
+  parameter DADO_ZERO_UM = 'b0110; 
+  parameter DADO_UM_ZERO = 'b1001; 
+  parameter DADO_UM_UM = 'b1100;
+
+  logic [1:0] entrada;
+
+  always_comb begin
+    entrada <= SWI[3:2];
+
+    if (entrada == 'b00) LED[7:4] <= DADO_ZERO_ZERO;
+    else if (entrada == 'b01) LED[7:4] <= DADO_ZERO_UM;
+    else if (entrada == 'b10) LED[7:4] <= DADO_UM_ZERO;
+    else LED[7:4] <= DADO_UM_UM;
+  end
+endmodule
